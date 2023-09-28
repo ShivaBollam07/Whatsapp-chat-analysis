7efc168cc7bc07f8d34126c8f2cb0f865220c36b
Shiva <98306948+ShivaBollam07@users.noreply.github.com>
2023-09-28 20:47:02 +0530

    First commit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-mkdir -p ~/.streamLit/
+mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
 port = $PORT\n\
-enableCORS =false\n\
-headLess = true\n\
+enableCORS = false\n\
+headless = true\n\
 \n\
-" >  ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
